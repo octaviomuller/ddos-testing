892ca807d66a105c5eef8840728560f7ae58d9d6
octaviomuller <octaviohmb@gmail.com>
2021-09-13 16:12:37 -0300

    refresh system

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var port = 4009
 
 let expiry = 1
 let fibCount = 1
+let lastDenial
+let newDenial
 
 function fib(n) {
   if (n <= 1) return 1
@@ -19,9 +21,17 @@ app.use(
     windowMs: 5000,
     max: 4,
     handler: async (req, res, next) => {
+      if (lastDenial === undefined) lastDenial = new Date()
       return await new Promise(async resolve => {
+        newDenial = new Date()
         setTimeout(() => {
+          console.log({ expiry, lastDenial, newDenial });
           expiry = fib(fibCount++)
+          if ((newDenial - lastDenial)/1000 >= 5) {
+            expiry = 1
+            fibCount = 1
+          }
+          lastDenial = newDenial
           resolve(next())
         }, expiry * 1000)
       })
@@ -31,6 +41,10 @@ app.use(
 
 app.get('/', (req, res) => {
   res.send('ok!')
+})
+
+app.get('/2', (req, res) => {
+  res.send('ok 2!')
 })
 
 app.listen(port, () => {
